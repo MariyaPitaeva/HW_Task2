030831232cfd2ae397c9f29a769f46ad75edf52b
Mariya Pitaeva <rada353@yandex.ru>
2024-07-18 13:35:16 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,39 @@
-﻿﻿// Задача 3.
-// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
-// Использовать рекурсию, не использовать циклы.
+﻿﻿// Задача 2.
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// A(m, n)
+// n + 1, при m = 0;
+// A(m - 1, 1), при m > 0, n = 0;
+// A(m - 1, A(m, n - 1)), при m > 0, n > 0;
 
 void Main()
 {
-    int[] array = { 8, 12, 37, 0, 4 };
-    ReverseArr(array, array.Length - 1);
+    int m = 3;
+    int n = 5;
 
-
+    Console.WriteLine($"A({m}, {n}) = {AkkerFunc(m, n)}");
 }
 
-void ReverseArr(int[] array, int i)
+int AkkerFunc(int m, int n)
 {
-    if (i < 0) return;
-    Console.Write(array[i] + " ");
-    ReverseArr(array, i - 1);
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return AkkerFunc(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return AkkerFunc(m - 1, AkkerFunc(m, n - 1));
+    }
+    return(AkkerFunc(m,n));
+
 }
 
 Main();
